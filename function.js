e91b1debcce7df1b3a3cc9ac188c133bbe1aa016
7Zeb <66693249+7zeb@users.noreply.github.com>
2025-05-12 20:37:21 -0500

    added negative number support

--- function.js
+++ function.js
@@ -3,7 +3,11 @@ let currentOperation = '';
 let previousInput = '';
 
 function appendNumber(number) {
-    currentInput += number;
+    if (currentInput === '' && number === '-') { // Allow negative numbers
+        currentInput = '-';
+    } else {
+        currentInput += number;
+    }
     document.getElementById('display').value = `${previousInput} ${currentOperation} ${currentInput}`;
 }
 
